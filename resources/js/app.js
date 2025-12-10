9232dd69b41b8dffc0a8c73edf7ab591b8f7e87c
teddy89PJS <teddygadlawan@gmail.com>
2025-12-10 16:38:14 +0800

    Fullcalendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+
 /*
   Add custom scripts here
 */
@@ -7,7 +8,10 @@ import.meta.glob([
   // '../assets/json/**',
   '../assets/vendor/fonts/**'
 ]);
+
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import '@fullcalendar/core/main.min.css';
-import '@fullcalendar/daygrid/main.min.css';
+
+// ✅ Updated FullCalendar CSS imports for v6
+import '@fullcalendar/core/index.css';
+import '@fullcalendar/daygrid/index.css';
